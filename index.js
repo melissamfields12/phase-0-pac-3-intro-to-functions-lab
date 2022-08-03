b7f7790cdb7261aac131a2c62a0b856cf3ed1f4c
Melissa Fields <melissamfields12@gmail.com>
2022-08-03 15:06:50 -0700

    commit tree d13a0d69400ca093ac557b0de22af2c8d453def2

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    const spy = 'HELLO';
+    console.log(string);
+    console.log(spy)
+}
+'hello'.toUpperCase();
+function logWhisper(string) {
+    const spy = 'hello';
+    console.log(string);
+    console.log(spy)
+    }
+    'HELLO'.toLowerCase();
+    
+    function sayHiToHeadphonedRoommate(string) {
+        if (string === string.toLowerCase()) {
+            return "I can\'t hear you!"
+        } 
+        if (string === string.toUpperCase()) {
+            return "YES INDEED!"
+        }
+        if (string === "Let\'s have dinner together!") {
+            return "I would love to!"
+        }
+        /* let greeting = "hello";
+        let tooLoud = "HELLO";
+        let invitation = "Let\'s have dinner together!";
+        greeting === greeting.toLowerCase()? "I can\'t hear you!" : "I cant hear you!";
+        tooLoud === tooLoud.toUpperCase()? "YES INDEED!" : "Yes indeed!"
+        invitation === "Let\'s have dinner together!"? "I would love to!" : "I WOULD LOVE TO!" */
+            }
+        
+         
+        
